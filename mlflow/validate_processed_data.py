9a18190b1a10fd69c16bec0532d19419aa63d508
Sophie Marie Bayersdörfer <134285528+Soph2325@users.noreply.github.com>
2025-03-16 18:24:08 +0100

    Update validate_processed_data.py

--- mlflow/validate_processed_data.py
+++ mlflow/validate_processed_data.py
@@ -1,7 +1,7 @@
 import great_expectations as gx
 import pandas as pd
 
-df = pd.read_csv('../data/processed/processed_data.csv')
+df = pd.read_csv('data/processed/processed_data.csv')
 
 context = gx.get_context()
 data_source = context.data_sources.add_pandas(name = "my_pandas_datasource")
