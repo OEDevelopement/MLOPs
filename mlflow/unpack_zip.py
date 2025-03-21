d4dcd44aabf863b0594f4a4c023d5fcd72897903
Sophie B. <sophie@bayersdoerfer.com>
2025-03-21 19:37:25 +0100

    Update zip unpacking

--- mlflow/unpack_zip.py
+++ mlflow/unpack_zip.py
@@ -2,7 +2,7 @@ import zipfile
 import os
 
 # unpack ZIP-file
-with zipfile.ZipFile("data/raw/adult-income-dataset.zip", "r") as zip_ref:
+with zipfile.ZipFile("mlflow/data/raw/adult-income-dataset.zip", "r") as zip_ref:
     zip_ref.extractall("data/raw")
 
 # delete ZIP-file
